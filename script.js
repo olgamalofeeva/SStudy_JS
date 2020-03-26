19a73ccf2e6d0af6ed99561a09dc0429c0ca5390
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-03-27 00:00:50 +0300

    Первый комит

---
+++ script.js
@@ -0,0 +1,10 @@
+var money = 500;
+var income = 1000;
+var addExpenses = 500;
+var deposit = 500;
+var mission = 'goal';
+var period = 'year';
+
+alert ('My dreames!');
+console.log ('are coming');
+
