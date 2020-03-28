bcb9b7012bc7f6116b76262569c22fe0125aeec9
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-03-28 17:33:57 +0300

    Второе домашнее задание

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-var money = 500;
-var income = 1000;
-var addExpenses = 500;
-var deposit = 500;
-var mission = 'goal';
-var period = 'year';
+var money = 50000;
+console.log (typeof money);
 
-alert ('My dreames!');
-alert ('are coming');
+var income = 'акции';
+console.log (typeof income);
+
+var addExpenses = 'Такси, Магазины, Техника';
+console.log (addExpenses.length);
+console.log (addExpenses.toLowerCase());
+console.log (addExpenses.split(', '));
+
+var deposit = true;
+console.log (typeof deposit);
+
+var mission = 1000000;
+console.log ('Цель заработать ' + mission + ' рублей');
+
+var period = 2;
+console.log ('Период равен ' + period + ' месяца');
+
+var budgetDay;
+budgetDay = money / 30;
+console.log ('budgetDay: ', budgetDay);
+
+
+
+
+
+
+
+
 
