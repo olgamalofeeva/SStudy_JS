f7ac02ebd2ea0a4e046b41273d93331301fe38e7
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-03-29 11:51:33 +0300

    Усложненное задание

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ var budgetDay;
 budgetDay = money / 30;
 console.log ('budgetDay: ', budgetDay);
 
+var num = '266219';
+console.log((num[0] * num[1] * num[2] * num[3] * num[4] * num[5])**3);
+
+var num2 = '2176782336';
+console.log(num2.substring(0,2));
+
+
 
 
 
