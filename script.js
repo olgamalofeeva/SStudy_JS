f76c3478cb73dd775e6a4d5877a12b3774906330
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-03-31 15:14:45 +0300

    Третья домашка

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
-var money = 50000;
-console.log (typeof money);
+let money = +prompt ('Ваш месячный доход?');
+console.log (money);
 
-var income = 'акции';
+let income = 'акции';
 console.log (typeof income);
 
-var addExpenses = 'Такси, Магазины, Техника';
-console.log (addExpenses.length);
-console.log (addExpenses.toLowerCase());
-console.log (addExpenses.split(', '));
+let addExpenses = +prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log (addExpenses);
 
-var deposit = true;
-console.log (typeof deposit);
+let deposit = confirm ('Есть ли у вас депозит в банке?');
+console.log (deposit);
 
-var mission = 1000000;
+let mission = 1000000;
 console.log ('Цель заработать ' + mission + ' рублей');
 
-var period = 2;
+let period = 2;
 console.log ('Период равен ' + period + ' месяца');
 
-var budgetDay;
-budgetDay = money / 30;
-console.log ('budgetDay: ', budgetDay);
-
-var num = '266219';
+let num = '266219';
 console.log((num[0] * num[1] * num[2] * num[3] * num[4] * num[5])**3);
 
-var num2 = '2176782336';
+let num2 = '2176782336';
 console.log(num2.substring(0,2));
 
+let expenses1 = prompt ('Введите обязательную статью расходов?');
+let amount1 = prompt ('Во сколько это обойдется?');
+let expenses2 = prompt ('Введите обязательную статью расходов?');
+let amount2 = prompt ('Во сколько это обойдется?');
 
+let budgetMonth;
+budgetMonth = money - amount1 - amount2;
+console.log('budgetMonth: ', budgetMonth);
+
+let goal;
+goal = mission/budgetMonth;
+console.log(Math.ceil(goal));
+
+let budgetDay;
+budgetDay = budgetMonth/ 30;
+console.log (Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay > 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+    console.log('Что то пошло не так');  
+}
 
 
 
