b2e34990aebe478e0ec9c3560d163b7d13976d32
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-03-31 16:03:52 +0300

    Дополнение к домашке

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-let money = +prompt ('Ваш месячный доход?');
-console.log (money);
-
 let income = 'акции';
 console.log (typeof income);
-
-let addExpenses = +prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log (addExpenses);
 
 let deposit = confirm ('Есть ли у вас депозит в банке?');
 console.log (deposit);
@@ -22,6 +16,13 @@ console.log((num[0] * num[1] * num[2] * num[3] * num[4] * num[5])**3);
 let num2 = '2176782336';
 console.log(num2.substring(0,2));
 
+let money;
+money = +prompt ('Ваш месячный доход?')
+console.log (money);
+
+let addExpenses = +prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log (addExpenses);
+
 let expenses1 = prompt ('Введите обязательную статью расходов?');
 let amount1 = prompt ('Во сколько это обойдется?');
 let expenses2 = prompt ('Введите обязательную статью расходов?');
@@ -29,15 +30,15 @@ let amount2 = prompt ('Во сколько это обойдется?');
 
 let budgetMonth;
 budgetMonth = money - amount1 - amount2;
-console.log('budgetMonth: ', budgetMonth);
+console.log('Бюджет на месяц' + ' ' + budgetMonth);
 
 let goal;
 goal = mission/budgetMonth;
-console.log(Math.ceil(goal));
+console.log('Цель будет достигунта за:' + ' ' + Math.ceil(goal) + ' ' + 'месяцев');
 
 let budgetDay;
 budgetDay = budgetMonth/ 30;
-console.log (Math.floor(budgetDay));
+console.log ('Бюджет на день:' + ' ' + Math.floor(budgetDay));
 
 if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
