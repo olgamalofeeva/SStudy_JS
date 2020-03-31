1dcf9adf0073bd15bbcd52a885641a0a71baa506
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-03-31 18:51:19 +0300

    level up

--- new/main.js
+++ new/main.js
@@ -18,8 +18,12 @@ switch (lang) {
         console.log (lang_en);   
 }
 
-let result = lang === true ? lang_ru:lang_en;
-console.log (result);
+const language = {
+    en: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday',
+    ru: 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
+};
+
+console.log(language.en);
 
 let namePerson = 'Кирилл';
 
@@ -28,19 +32,5 @@ console.log(result01);
 let result02 = namePerson === 'Максим' ? 'преподаватель':'студент';
 console.log(result02);
 
-var arr_1 = new Array('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-var arr_2 = new Array('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-for(var i = 0; i < [1,2]; i++) arr_1[i] = i;
-for(var i = 0; i < [1,2,3]; i++) arr_2[i] = i;
 
-var arr = new Array(arr_1, arr_2);
 
-//Первый цикл проходит три раза переберая элементы массива arr
-/*Второй цикл переберает количество елементов во вложеном массиве и так как первый цикл проходит три 
-раза то второй проходит 3 * arr[x].length раз.*/
-for(var x = 0; x < arr.length; x++){
-	for(var j = 0; j < arr[x].length; j++) {
-		document.write(arr[x][j] + " ");
-	}
-	document.write("<br />");
-}
