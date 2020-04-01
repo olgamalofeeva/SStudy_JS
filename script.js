451a121de07c3f9cc288075d88c7e85efbe7242f
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-04-01 12:09:28 +0300

    4 lesson

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-let income = 'акции';
-console.log (typeof income);
+let money;
+money = +prompt ('Ваш месячный доход?')
+console.log ('Мой месячный доход:' + ' ' + money);
 
-let deposit = confirm ('Есть ли у вас депозит в банке?');
-console.log (deposit);
+let expenses1 = prompt ('Введите обязательную статью расходов?');
+let amount1 = +prompt ('Во сколько это обойдется?');
+let expenses2 = prompt ('Введите обязательную статью расходов?');
+let amount2 = +prompt ('Во сколько это обойдется?');
+
+let addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log ('Мои возможные доходы за месяц:' + ' ' + String(addExpenses));
+
+const getExpensesMonth = function (a, b) {
+    return a + b;
+};
+console.log('Мои обязательные расходы за месяц:' + ' ' + getExpensesMonth(amount1, amount2));
+
+
+function getAccumulatedMonth (a, b, c) {
+    return a - (b + c);
+};
+
+let accumulatedMonth = getAccumulatedMonth(money, amount1, amount2);
+console.log('Мой доход:' + ' ' + accumulatedMonth);
 
 let mission = 1000000;
 console.log ('Цель заработать ' + mission + ' рублей');
 
-let period = 2;
-console.log ('Период равен ' + period + ' месяца');
+const getTargetMonth = function (a, b) {
+    return a / b;
+};
 
-let num = '266219';
-console.log((num[0] * num[1] * num[2] * num[3] * num[4] * num[5])**3);
-
-let num2 = '2176782336';
-console.log(num2.substring(0,2));
-
-let money;
-money = +prompt ('Ваш месячный доход?')
-console.log (money);
-
-let addExpenses = +prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log (addExpenses);
-
-let expenses1 = prompt ('Введите обязательную статью расходов?');
-let amount1 = prompt ('Во сколько это обойдется?');
-let expenses2 = prompt ('Введите обязательную статью расходов?');
-let amount2 = prompt ('Во сколько это обойдется?');
-
-let budgetMonth;
-budgetMonth = money - amount1 - amount2;
-console.log('Бюджет на месяц' + ' ' + budgetMonth);
-
-let goal;
-goal = mission/budgetMonth;
-console.log('Цель будет достигунта за:' + ' ' + Math.ceil(goal) + ' ' + 'месяцев');
+console.log('Цель будет достигунта за:' + ' ' + Math.ceil(getTargetMonth(mission, accumulatedMonth)) + ' ' + 'месяцев');
 
 let budgetDay;
-budgetDay = budgetMonth/ 30;
+budgetDay = accumulatedMonth/ 30;
 console.log ('Бюджет на день:' + ' ' + Math.floor(budgetDay));
 
 if (budgetDay >= 1200) {
@@ -49,22 +45,5 @@ if (budgetDay >= 1200) {
 } else {
     console.log('Что то пошло не так');  
 }
-
-const getExpensesMonth = function (a, b) {
-    return a + b;
-};
-
-console.log(getExpensesMonth(Number(amount1), Number(amount2)));
-
-
-function getAccumulatedMonth (a, b, c) {
-    return a - (b + c);
-};
-
-let accumulatedMonth = getAccumulatedMonth(Number(money), Number(amount1), Number(amount2));
-console.log(accumulatedMonth);
-
-
-
 
 
