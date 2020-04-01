de3a3d893603d9fe5192050e838ab4b881ea73e3
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-04-01 14:10:33 +0300

    new getStatusIncome

--- new/main.js
+++ new/main.js
@@ -1,36 +1,9 @@
-let lang_en = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
-let lang_ru = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
-
-let lang = confirm('Ваш язык русский?');
-console.log(lang);
-
-if (lang === true) {
-    console.log(lang_ru);
-} else if (lang === false) {
-    console.log(lang_en);
-}
-
-switch (lang) {
-    case true:
-        console.log (lang_ru);
-        break;
-    default:
-        console.log (lang_en);   
-}
-
-const language = {
-    en: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday',
-    ru: 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
+const showMessage = function (a) {
+   if (typeof a = String) {
+       return a;
+} else {return 'функция передана неверно'};
 };
 
-console.log(language.en);
+let a = 5;
 
-let namePerson = 'Кирилл';
-
-let result01 = namePerson === 'Артем' ? 'директор':'студент';
-console.log(result01);
-let result02 = namePerson === 'Максим' ? 'преподаватель':'студент';
-console.log(result02);
-
-
-
+console.log(showMessage);
